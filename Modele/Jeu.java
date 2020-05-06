2b1c8a130b078e3b34e53a354a372bef7fd6dc46
thecanarddefer <32815841+thecanarddefer@users.noreply.github.com>
2020-05-06 13:46:14 +0200

    config connecté partielement

--- Modele/Jeu.java
+++ Modele/Jeu.java
@@ -6,8 +6,11 @@ import java.util.Stack;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 
@@ -26,11 +29,50 @@ public class Jeu extends Observable {
 	Couleur atout;// l'atout de la manche
 	Carte c_dom;// carte jouer par le joueur dominant 
 	Carte c_sub;// carte jouer par l'autre joueur
+	int diff;
 	
 
 	public Jeu() {
-		parManche=false;//////// condition par défault
-		totalfin=100;///////////
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("config"));
+			diff = Integer.parseInt(br.readLine());
+			if (diff < 0 || diff > 1) { // a changer pour diff > 2
+				diff = 0;
+			}
+			if(Integer.parseInt(br.readLine())==1) {
+				parManche=true;
+			}
+			
+			totalfin = Integer.parseInt(br.readLine());
+//			selCarte = Integer.parseInt(br.readLine());
+//			if (selCarte < 0 || selCarte > 2) {
+//				selCarte = 0;
+//			}
+//			selFond = Integer.parseInt(br.readLine());
+//			if (selFond < 0 || selFond > 2) {
+//				selFond = 0;
+//			}
+			br.close();
+		} catch (IOException e) {// si le fichier n'est pas trouvé on en crée un avec les configurations de base
+			try {
+				File confile = new File("config");
+				if (confile.createNewFile()) {
+					System.out.println("Création d'un fichier config.");
+					FileWriter myWriter = new FileWriter("config");
+					myWriter.write("0\n0\n100\n0\n0\n");
+					myWriter.close();
+					diff = 0;
+					parManche = false;
+					totalfin = 100;
+//					selCarte = 0;
+//					selFond = 0;
+				}
+			} catch (IOException e1) {// la création du fichier a echoué
+				System.out.println("impossible de créer un fichier.");
+			}
+		}
+//		parManche=false;//////// condition par défault
+//		totalfin=100;///////////
 		enCours = true;
 		changerjoueur=false;
 		joueurdominant=0;
