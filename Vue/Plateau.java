0b6a91193034f54ae51c95ce51b6a496388fe719
etienne <dode.etienne@gmail.com>
2020-05-08 16:13:19 +0200

    feedforward

--- Vue/Plateau.java
+++ Vue/Plateau.java
@@ -6,8 +6,10 @@ import Patterns.Observateur;
 
 import javax.swing.*;
 import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.*;
 import java.io.InputStream;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
@@ -27,17 +29,18 @@ public class Plateau extends JPanel implements Observateur {
     //Dimensions a revoir, adapter a la taille de l'écran
     private Dimension dimlabel;
     private int etapePrecedente;
-    private int manchePrec = 0;
+    private int manchePrec = 1;
     private boolean chgtManche = false;
-    private int lastnbPlis1=0;
-    private int lastnbPlis2=0;
+    private int lastnbPlis1 = 0;
+    private int lastnbPlis2 = 0;
+    private JLabel indPioche;
 
     public Plateau(Jeu j, CollecteurEvenements c, Menu m) {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         dimlabel = new Dimension(screenSize.width / 25, screenSize.height / 10);
         this.m = m;
         this.c = c;
-       
+
         jeu = j;
         //On ajoute le plateau dans la liste des observateur
         //Les observateurs seront mis à jour par le jeu dès que nécessaire
@@ -53,7 +56,7 @@ public class Plateau extends JPanel implements Observateur {
 //        majMainJoueur(1);
 //        majCarteJouees();
 //        this.m.indiqueAtout(jeu.getAtout().name());
-        
+
         //repaint();
     }
 
@@ -86,7 +89,7 @@ public class Plateau extends JPanel implements Observateur {
         space.setPreferredSize(new Dimension(dimlabel.width, dimlabel.height / 2));
         this.add(space);
 
-        JLabel indPioche = new JLabel("Pioches");
+        indPioche = new JLabel("Pioches");
         this.add(indPioche);
         JPanel paquetCentrePane = new JPanel();
         for (int i = 0; i < 6; i++) {
@@ -165,22 +168,34 @@ public class Plateau extends JPanel implements Observateur {
 
         for (int i = 0; i < 11; i++) {
             if (i < jeu.getMains()[numJ].getnbCarte()) {
-                Icon img;
+
                 //Si on veut montrer les cartes ou non
-                if (numJ != jeu.joueurActuelle()) {
-                    img = new ImageIcon(new ImageIcon(ClassLoader.getSystemClassLoader().getResource("Back Blue 1.png")).getImage().getScaledInstance(dimlabel.width, dimlabel.height, Image.SCALE_SMOOTH));
-
-                } else {
-                    img = new ImageIcon(new ImageIcon(ClassLoader.getSystemClassLoader().getResource(jeu.getMains()[numJ].getMain()[i].getResourceName())).getImage().getScaledInstance(dimlabel.width, dimlabel.height, Image.SCALE_SMOOTH));
-
-                }
-                hands.get(numJ).get(i).setIcon(img);
+//                if (numJ ==1 && jeu.getIA()) {
+//                    icon = new ImageIcon(new ImageIcon(ClassLoader.getSystemClassLoader().getResource("Back Blue 1.png")).getImage().getScaledInstance(dimlabel.width, dimlabel.height, Image.SCALE_SMOOTH));
+//
+//                } else {
+//                    icon = new ImageIcon(new ImageIcon(ClassLoader.getSystemClassLoader().getResource(jeu.getMains()[numJ].getMain()[i].getResourceName())).getImage().getScaledInstance(dimlabel.width, dimlabel.height, Image.SCALE_SMOOTH));
+//
+//                }
+                //Icon img = new ImageIcon(GrayFilter.createDisabledImage(icon.getImage()));
+                //hands.get(numJ).get(i).setIcon(img);
+                ImageIcon icon2 = new ImageIcon(new ImageIcon(ClassLoader.getSystemClassLoader().getResource(jeu.getMains()[numJ].getMain()[i].getResourceName())).getImage().getScaledInstance(dimlabel.width, dimlabel.height, Image.SCALE_SMOOTH));
+                hands.get(numJ).get(i).setIcon(icon2);
                 if ((jeu.etape() == 0 || jeu.etape() == 1) && jeu.joueurActuelle() == numJ && jeu.peutJouer(i, numJ)) {
+                   
                     if (hands.get(numJ).get(i).getMouseListeners().length == 0) {
                         hands.get(numJ).get(i).addMouseListener(new JoueurCarteListener(i, c));
+                       
                     }
-                } else if (hands.get(numJ).get(i).getMouseListeners().length > 0) {
-                    hands.get(numJ).get(i).removeMouseListener(hands.get(numJ).get(i).getMouseListeners()[0]);
+                } else {
+                    if((jeu.etape() == 0 || jeu.etape() == 1) && jeu.joueurActuelle() == numJ && !jeu.peutJouer(i, numJ)){
+                        //ImageIcon icon = new ImageIcon(new ImageIcon(ClassLoader.getSystemClassLoader().getResource(jeu.getMains()[numJ].getMain()[i].getResourceName())).getImage().getScaledInstance(dimlabel.width, dimlabel.height, Image.SCALE_SMOOTH));
+                        Icon img = new ImageIcon(GrayFilter.createDisabledImage(icon2.getImage()));
+                        hands.get(numJ).get(i).setIcon(img);
+                    }
+                    if (hands.get(numJ).get(i).getMouseListeners().length > 0) {
+                        hands.get(numJ).get(i).removeMouseListener(hands.get(numJ).get(i).getMouseListeners()[0]);
+                    }
                 }
             } else {
                 hands.get(numJ).get(i).setIcon(null);
@@ -189,7 +204,11 @@ public class Plateau extends JPanel implements Observateur {
     }
 
     private void majPaquets() {
-
+        if(jeu.etape()==2 || jeu.etape()==3){
+            indPioche.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
+        } else {
+            indPioche.setBorder(null);
+        }
         for (int i = 0; i < 6; i++) {
             if (jeu.getPiles()[i].estVide()) {
                 centreDecks.get(i).setIcon(null);
@@ -225,8 +244,10 @@ public class Plateau extends JPanel implements Observateur {
             //version texte
             //joue1.setText(jeu.getC_dom().toString());
             //version image
-            Icon img = new ImageIcon(new ImageIcon(ClassLoader.getSystemClassLoader().getResource(jeu.getC_dom().getResourceName())).getImage().getScaledInstance(dimlabel.width, dimlabel.height, Image.SCALE_SMOOTH));;
-            playedCards.get(0).setIcon(img);
+            //ImageIcon icon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource(jeu.getC_dom().getResourceName()));;
+            ImageIcon icon = new ImageIcon(new ImageIcon(ClassLoader.getSystemClassLoader().getResource(jeu.getC_dom().getResourceName())).getImage().getScaledInstance(dimlabel.width, dimlabel.height, Image.SCALE_SMOOTH));
+
+            playedCards.get(0).setIcon(icon);
         }
         if (jeu.etape() == 2) {
             //version texte
@@ -257,16 +278,17 @@ public class Plateau extends JPanel implements Observateur {
         JPanel finManchePane = new JPanel();
         finManchePane.setLayout(new BoxLayout(finManchePane, BoxLayout.PAGE_AXIS));
         finManchePane.setFont(new Font("Calibri", Font.PLAIN, 28));
-        JLabel finJ = new JLabel("La manche " + (jeu.getMancheactuelle()-1) + " est terminee");
+        JLabel finJ = new JLabel("La manche " + (jeu.getMancheactuelle() - 1) + " est terminee");
         finJ.setFont(new Font("Calibri", Font.PLAIN, 28));
         finManchePane.add(finJ);
         JLabel finRemporte;
-        if(lastnbPlis1>lastnbPlis2)   
-            finRemporte = new JLabel("Remportee par le joueur 1 avec "+lastnbPlis1+" plis a "+lastnbPlis2);
-        else
-            finRemporte = new JLabel("Remportee par le joueur 2 avec "+lastnbPlis2+" plis a "+lastnbPlis1);
+        if (lastnbPlis1 > lastnbPlis2) {
+            finRemporte = new JLabel("Remportee par le joueur 1 avec " + lastnbPlis1 + " plis a " + lastnbPlis2);
+        } else {
+            finRemporte = new JLabel("Remportee par le joueur 2 avec " + lastnbPlis2 + " plis a " + lastnbPlis1);
+        }
         finManchePane.add(finRemporte);
-        
+
         JButton contBout = new JButton("Continuer la partie");
         finManchePane.add(contBout);
         this.add(finManchePane);
