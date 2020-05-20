49693c8a409ac8d26b0d0038b402546c1725a919
corentin8 <58816557+corentin8@users.noreply.github.com>
2020-05-20 14:27:21 +0200

    derniere version

--- Controleur/IAMin_Max.java
+++ Controleur/IAMin_Max.java
@@ -24,11 +24,13 @@ public class IAMin_Max extends Joueur {
 		for (int i=0;i<jeu.getMains()[num].getnbCarte();i++) {
 			//met a jour la position des carte qu'on a en main
 			contexte.Carte_Main(jeu.getMains()[num].voirCarte(i));
+			System.out.println("init carte "+jeu.getMains()[num].voirCarte(i)+"carte num"+i);
 			CarteMain[i]=contexte.CarteAInt(jeu.getMains()[num].voirCarte(i));
+			
 		}
 		//cree notre noeud de depart
 		arbre.arbreMinMax(1,contexte,jeu.j_dom(),jeu.etape(),num,12,carte,CarteMain,0);
-		creeArbre(arbre,arbre.getetage()+etageSup());//indique la profondeur voulu pour l'arbre
+		creeArbre(arbre,arbre.getetage()+1);//indique la profondeur voulu pour l'arbre
 		int tmp;
 		tmp=comptefils(arbre);
 		etape=0;
@@ -73,19 +75,19 @@ public class IAMin_Max extends Joueur {
 			e=3;
 		}
 		else if(inconnue>17){
-			e=4;
+			e=3;
 		}
 		else if(inconnue>13) {
-			e=5;
+			e=3;
 		}
 		else if(inconnue>10) {
-			e=6;
+			e=4;
 		}
 		else if(inconnue>5) {
-			e=8;
+			e=4;
 		}
 		else {
-			e=10;
+			e=6;
 		}
 		return e;
 	}
@@ -95,11 +97,7 @@ public class IAMin_Max extends Joueur {
 		int j;
 		Carte Ajouer;
 		arbreMinMax fils[];
-		//int tmp[];
-		creeArbre(arbre,arbre.getetage()+3);//indique la profondeur voulu pour l'arbre
-		/*tmp=comptefils(arbre);
-		System.out.println("il y a actuellement "+tmp[0]+" de fils crée");
-		System.out.println("il en sont a l'etape "+tmp[1]);*/
+		creeArbre(arbre,arbre.getetage()+etageSup());//indique la profondeur voulu pour l'arbre
 		switch (jeu.etape()) {
 		case 0:
 		case 1:
@@ -111,6 +109,9 @@ public class IAMin_Max extends Joueur {
 			Ajouer=tmp.getCarteAction();
 			i=indiceCarteMain(Ajouer);
 			etape=jeu.etape();
+			nextetape();
+			creeArbre(arbre,arbre.getetage()+etageSup());
+			avancer_arbre(Ajouer);
 			break;
 		case 2:
 		case 3:
@@ -125,7 +126,6 @@ public class IAMin_Max extends Joueur {
 	
 	
 	int meilleurPioche() {
-		//System.out.println("recherche meilleur pioche");
 		Deck pioche[];
 		pioche=jeu.getPiles();
 		arbreMinMax tmp;
@@ -136,10 +136,8 @@ public class IAMin_Max extends Joueur {
 		for (int j=0;j<arbre.getnbfils();j++) {
 			tmp=fils[j];
 			for (int i=0;i<6;i++) {
-				//System.out.println("regarde si la pioche est piochable");
 					if(jeu.peutPiocher(i)) {
 						if(pioche[i].topDeck().getCouleur()==tmp.getCarteAction().getCouleur() && pioche[i].topDeck().getValeur()==tmp.getCarteAction().getValeur()) {
-							//System.out.println("la pioche est piochable");
 							if(tmp.gettauxvictoir()>meilleur) {
 								meilleur=tmp.gettauxvictoir();
 								ind=i;
@@ -152,6 +150,7 @@ public class IAMin_Max extends Joueur {
 	}
 	
 	public int indiceCarteMain(Carte c) {
+		//renvoi l'indice de la carte donner dans la main
 		int i=0;
 		boolean trouver=false;
 		while(i<jeu.getMains()[num].getnbCarte() && !trouver) {
@@ -212,6 +211,28 @@ public class IAMin_Max extends Joueur {
 	return fin;
 	}
 	
+	void nextetape() {
+		switch(etape) {
+		case 0:
+			etape=1;
+			break;
+		case 1:
+			if(jeu.pilesvide()) {
+				etape=0;
+			}
+			else {
+				etape=2;
+			}
+			break;
+		case 2:
+			etape=3;
+			break;
+		case 3:
+			etape=0;
+			break;
+		}
+	}
+	
 	public void deplace_arbre() {
 		//trouve la carte jouer a l'etape qui nous interesse
 		Carte jouer=null;
@@ -221,22 +242,12 @@ public class IAMin_Max extends Joueur {
 			switch(arbre.getetape()) {
 			case 0:
 				//System.out.println("carte poser premier ");
-				if(finpioche && arbre.getdom()!=jeu.j_dom() && jeu.j_dom()!=num) {
-					jouer=jeu.getc_doma();
-				}
-				else {
 					jouer=jeu.getC_dom();
-				}
 				etape=1;
 				break;
 			case 1:
 				//System.out.println("carte poser second ");
-				if(finpioche && arbre.getdom()!=jeu.j_dom() && jeu.j_dom()!=num) {
-					jouer=jeu.getc_suba();
-				}
-				else {
 					jouer=jeu.getC_sub();
-				}
 				if(jeu.pilesvide()) {
 					etape=0;
 				}
